6e906f8e28c3310cdb397d46a13f942dee7b76fe
Gowynn Hunt <gowynnhunt@gmail.com>
2024-07-06 20:13:12 -0500

    Miscellaneous changes

--- script/main.js
+++ script/main.js
@@ -14,10 +14,26 @@ console.log(quizQuestions[0].question);
 const dogQuiz = document.querySelector("#corgi-btn");
 const modalClose = document.querySelector(".modal-close");
 
+function blurNodes(selector) {
+  const nodes = document.querySelectorAll(selector);
+  for (const node of nodes) {
+    node.style.filter = "blur(2.5px)";
+  }
+}
+
+function unBlurNodes(selector) {
+  const nodes = document.querySelectorAll(selector);
+  for (const node of nodes) {
+    node.style.filter = "blur(0)";
+  }
+}
+
 dogQuiz.addEventListener("click", () => {
   document.querySelector(".modal").style.display = "flex";
+  blurNodes(".container");
 });
 
 modalClose.addEventListener("click", () => {
   document.querySelector(".modal").style.display = "none";
+  unBlurNodes(".container");
 });
