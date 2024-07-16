ac2e3947884e75b0cf8d940244ac17b30fc16b69
Gowynn Hunt <gowynnhunt@gmail.com>
2024-07-16 12:40:50 -0500

    Feature quiz buttons dynamically appear

--- script/main.js
+++ script/main.js
@@ -2,6 +2,7 @@ import { score, results, dogQuizQuestions } from "./data.js";
 import { getQuizArray } from "./quizFuncs.js";
 
 let quizArray = [];
+let choices = [];
 let idx = 0;
 
 const dogBtn = document.querySelector("#dog-btn");
@@ -27,40 +28,69 @@ function unBlurNodes(selector) {
   }
 }
 
-function openModal() {
+function startQuiz() {
   modal.style.display = "flex";
   blurNodes(".container");
 
   quizArray = getQuizArray(dogQuizQuestions);
+  choices = [];
+  idx = 0;
+  quizButtonHandler();
   quiz.replaceChildren(quizArray[0]);
 }
 
-function closeModal() {
+function exitQuiz() {
   modal.style.display = "none";
   unBlurNodes(".container");
 }
 
 function nextQuizQuestion() {
-  if (quizArray[idx + 1] === undefined) {
-    return;
-  } else {
-    idx += 1;
-    quiz.replaceChildren(quizArray[idx]);
-  }
+  idx++;
+  quizButtonHandler();
+  quiz.replaceChildren(quizArray[idx]);
 }
 
 function previousQuizQuestion() {
-  if (quizArray[idx - 1] === undefined) {
-    return;
+  idx--;
+  quizButtonHandler();
+  quiz.replaceChildren(quizArray[idx]);
+}
+
+function finishQuiz() {
+  exitQuiz();
+}
+
+function quizButtonHandler() {
+  if (
+    !choices.includes(undefined) &&
+    choices.length === dogQuizQuestions.length
+  ) {
+    finish.style.display = "block";
+  }
+
+  if (idx === 0) {
+    previous.style.display = "none";
   } else {
-    idx -= 1;
-    quiz.replaceChildren(quizArray[idx]);
+    previous.style.display = "block";
+  }
+
+  if (quizArray[idx + 1] === undefined) {
+    next.style.display = "none";
+  } else {
+    next.style.display = "block";
   }
 }
 
-dogBtn.addEventListener("click", openModal);
-modalClose.addEventListener("click", closeModal);
+dogBtn.addEventListener("click", startQuiz);
+modalClose.addEventListener("click", exitQuiz);
+
+quiz.addEventListener("click", (event) => {
+  if (event.target.tagName === "INPUT") {
+    choices[idx] = dogQuizQuestions[idx].values[event.target.value];
+    quizButtonHandler();
+  }
+});
 
 previous.addEventListener("click", previousQuizQuestion);
 next.addEventListener("click", nextQuizQuestion);
-// finish.addEventListener("click", finishQuiz);
+finish.addEventListener("click", finishQuiz);
