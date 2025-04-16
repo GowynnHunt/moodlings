e84be0d25005e1623ecaa2f7fd2c5ee1861decd3
Gowynn Hunt <gowynnhunt@gmail.com>
2025-04-15 19:38:52 -0500

    Still WIP on structure and styling

--- script/main.js
+++ script/main.js
@@ -1,18 +1,40 @@
-const aboutModal = document.querySelector("#About");
-const aboutUsButton = document.querySelector('footer a[href="#About"]');
+const root = document.querySelector(':root');
+const rootStyles = getComputedStyle(root);
 
-const themeSwitcher = document.querySelector("#theme-switcher");
-themeSwitcher.addEventListener("click", () => {
-  return
-})
+// Toggles modal if the modal exists
+function toggleModal(modal) {
+  if (modal == null || modal == undefined) {
+    console.log(`That Modal doesn't exist yet.\nIt's value is ${modal}.`);
+    return;
+  }
+  modal.style.display = modal.style.display == "" ? "flex" : "";
+  modal.classList.toggle("fade");
+  modal.firstElementChild.classList.toggle("bouncy");
+}
 
-aboutUsButton.addEventListener("click", () => toggleModal(aboutModal));
-aboutModal.addEventListener("click", (e) => {
-  if (e.target.id == "About") {
-    toggleModal(aboutModal)
+// Clicking outside of a modal closes it
+document.querySelectorAll("modal").forEach((modal) => {
+  modal.addEventListener("click", (e) => {
+    if (e.target.id == modal.id) {
+      toggleModal(modal);
+    }
+  });
+});
+
+// Footer links open respective modal
+document.querySelectorAll("footer > a").forEach((a) => {
+  if (a.hasAttribute("href") && a.getAttribute("href") != "") {
+    a.addEventListener("click", () => {
+      toggleModal(document.querySelector(a.getAttribute("href")));
+    });
   }
 });
 
-function toggleModal(modal) {
-  modal.style.display = modal.style.display == "" ? "flex" : ""
-}
+// Modal Header links change color when activated
+// const links = document.querySelectorAll(".modal-header a");
+// links.forEach((link) => link.addEventListener("click", () => {
+//   // Clear other highlighted links
+//   links.forEach((link) => (link.style.backgroundColor = "transparent"));
+//   // Highlight clicked link
+//   link.style.backgroundColor = rootStyles.getPropertyValue("--accent");
+// }))
